d80cb1842ece3bfe0fd914c01dd5d00f77faf826
dynamasoft <smithtanny@gmail.com>
2022-03-23 23:01:09 -0400

    added keypair.json for account

--- clients/src/App.js
+++ clients/src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import { Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import idl from './idl.json';
+import kp from './keypair.json'
 
 // SystemProgram is a reference to the Solana runtime!
 const { SystemProgram, Keypair } = web3;
 
 // Create a keypair for the account that will hold the GIF data.
-let baseAccount = Keypair.generate();
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
 
 // Get our program's id from the IDL file.
 const programID = new PublicKey(idl.metadata.address);
@@ -120,7 +123,12 @@ const App = () => {
     debugger;
 
     if (inputValue.length > 0) {
+      debugger;
       console.log('Gif link:', inputValue);
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      await program.rpc.addGif(inputValue);
+
       setGifList([...gifList, inputValue]);
       setInputValue('');
     } else {
@@ -162,6 +170,7 @@ const App = () => {
           <div className="connected-container">
             <form
               onSubmit={(event) => {
+                debugger;
                 event.preventDefault();
                 sendGif();
               }}
